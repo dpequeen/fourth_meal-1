0b07872cefdb04e77be701eeb5064d6814ff38ea
QuillyT <QuillyT@gmail.com>
2013-12-12 21:14:44 -0700

    Add Create Restaurant Test and add login/logout test.

--- spec/features/create_restaurant_spec.rb
+++ spec/features/create_restaurant_spec.rb
@@ -1,2 +1,25 @@
 require 'spec_helper'
 
+describe 'Create Restaurant' do
+  context 'Restaurant Admin' do
+    it 'can create a restaurant' do
+      visit '/'
+      click_on 'Login'
+      fill_in 'Email', with: "denvergschool@gmail.com"
+      fill_in 'Password', with: "password"
+      click_on 'Sign in'
+      click_on 'Request to create a new restaurant'
+      fill_in 'Name', with: 'MONKEY BALL'
+      fill_in 'Description', with: 'you need to ask?'
+      click_on 'Create Restaurant'
+      expect(page).to have_content('Thank you')
+      click_on 'Home'
+      click_on 'Logout'
+      click_on 'Login'
+      fill_in 'Email', with: "denvergschool@gmail.com"
+      fill_in 'Password', with: "password"
+      click_on 'Sign in'
+      expect(page).to have_content('MONKEY BALL')
+    end
+  end
+end
---
+++ spec/features/login_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'Login: ' do
+  context 'Platform Admin' do
+    it 'can log in and out of the platform site' do
+      visit '/'
+      click_on 'Login'
+      fill_in 'Email', with: "denvergschool@gmail.com"
+      fill_in 'Password', with: "password"
+      click_on 'Sign in'
+      expect(page).to have_content('Logout')
+      click_on 'Logout'
+      expect(page).to have_content('Login')
+    end
+  end
+end
